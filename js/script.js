41d4b60a75615b4000f6731d3346d5674ccdafde
Aliya Gizatullina <aliyusha.gizatullina@gmail.com>
2018-12-16 13:49:11 +0100

    Adding javascript to alert user

---
+++ js/script.js
@@ -0,0 +1,4 @@
+setTimeout(function() {
+	alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
+, 
+10000 /* wait 10 seconds before asking user. maybe he'll have time to read this amazing page */);
