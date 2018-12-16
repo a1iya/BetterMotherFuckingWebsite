f5bb305095cc352c2b93a42accf2712a87d5bc9d
Aliya Gizatullina <aliyusha.gizatullina@gmail.com>
2018-12-16 14:04:34 +0100

    Update Javascript, helping user to redirect to the xxxvidsxxx if he says ok to the popup

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-setTimeout(function() {
-	alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
-, 
+window.onload = setTimeout(function() {
+	if (confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please')) {
+		var url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'; // i know where to redirect the user
+		//window.location.href = url; // if i want the user to leave my wonderful page
+		window.open(url,'_blank'); // if i want my website stay but open the xxx link aside
+	}
+}, 
 10000 /* wait 10 seconds before asking user. maybe he'll have time to read this amazing page */);
